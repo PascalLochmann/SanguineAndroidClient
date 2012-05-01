c92e22dda45b479ddc05208f0d09ea9952a701cf
PascalLochmann <pascal.lochmann@googlemail.com>
2012-05-01 20:25:58 +0200

    Added test method to GameEngine

--- src/de/pl/sanguine/engine/GameEngine.java
+++ src/de/pl/sanguine/engine/GameEngine.java
@@ -2,4 +2,12 @@ package de.pl.sanguine.engine;
 
 public class GameEngine {
 
+	/*
+	 * Test if jenkins finds new code
+	 */
+	public int test(int i, int j) {
+		int x = 3;
+		
+		return x * i +j
+	}
 }
